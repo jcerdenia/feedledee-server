34e6ffdb52e3657504b08ab5386addab2323ead3
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-02-27 21:04:57 +0800

    Add check to register only unique emails

--- controllers/user.js
+++ controllers/user.js
@@ -3,12 +3,18 @@ const bcrypt = require("bcrypt");
 const token = require("../token");
 
 const register = (params) => {
-  const newUser = new User({
-    email: params.email,
-    password: bcrypt.hashSync(params.password, 10),
-  });
+  return User.find({ email: params.email }).then((result) => {
+    if (!result.length) {
+      const newUser = new User({
+        email: params.email,
+        password: bcrypt.hashSync(params.password, 10),
+      });
 
-  return newUser.save().then((_user, err) => (err ? false : true));
+      return newUser.save().then((_user, err) => (err ? false : true));
+    } else {
+      return { error: "User already exists." };
+    }
+  });
 };
 
 const auth = (params) => {
