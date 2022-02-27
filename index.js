45285fee0354cfae9e984b7032ac5bf4733b68a5
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-02-27 19:57:02 +0800

    Add connection to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (_req, res) => {
   res.send("It works!");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server listening on port", process.env.PORT);
+  console.log(`Server listening on port ${process.env.PORT}.`);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
